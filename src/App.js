2bee5de87df08e65054f0bd7af44c3cdfbe38a82
nahid <diegonahid@gmail.com>
2021-12-24 00:30:22 +0600

    material ui added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import "./css/purgeCss/index.css";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import Alert from "@mui/material/Alert";
+import { AlertTitle } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { green, red, yellow } from "@mui/material/colors";
+import { ThemeProvider } from "@emotion/react";
+
+const customTheme = createTheme({
+  palette: {
+    primary: {
+      main: yellow["A700"],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
 
 function App() {
-  return <div className="container bg-secondary text-blue-light-5">
-    
-  </div>;
+  const [versity, setVersity] = useState("");
+  useEffect(() => {
+    fetch("http://universities.hipolabs.com/search?country=bangladesh")
+      .then((response) => response.json())
+      .then((data) => setVersity(data[20]["name"]))
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+  return (
+    <ThemeProvider theme={customTheme}>
+      <div className="container bg-primary">
+        <Alert severity="info" color="primary">
+          <AlertTitle>Name:{versity}</AlertTitle>
+          <p>
+            website name : <strong>cuet.ac.bd</strong>
+          </p>
+        </Alert>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
